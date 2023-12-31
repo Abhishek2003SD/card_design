1c6ba0ca17431f74db91435ecc810bacc42da936
S.D.L Abhishek Pramuditha <153332055+Abhishek2003SD@users.noreply.github.com>
2023-12-31 06:12:47 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="myStyle.css">
+</head>
+<body>
+
+
+    <div class="main-box">
+        <div class="content">
+            <input type="checkbox" id="check">
+            <label for="check" class="box">
+                <div class="share">Show</div>
+                <div class="cancel">Hidden</div>
+            </label>
+            <div class="image-box">
+            <h4>Your Profile</h4>
+                <img src="styleImages\profile.png" alt="">
+                <div class="about">
+                    <div class="details">
+                        <div class="name">ABHISHEK PRAMUDITHA</div>
+                        <div class="job">Software developer</div>
+                        <div class="icon">
+                        <img src="styleImages\icon.png" alt="" id="i1">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
